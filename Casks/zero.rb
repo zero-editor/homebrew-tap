9c08f829abb040c37de40837034ea13c2b0eff9a
vidvidvid <weetopol@gmail.com>
2026-08-23 16:42:53 +0200

    zero 0.21.0

--- Casks/zero.rb
+++ Casks/zero.rb
@@ -1,6 +1,6 @@
 cask "zero" do
-  version "0.20.2"
-  sha256 "735d8a80a13922fe0906a542bf5944336c11dbb1811881ec35692f0aeead809b"
+  version "0.21.0"
+  sha256 "f655729f12bd8d2667c1252ab934ba0d4182b78aa740662577b45504729b8b1b"
 
   url "https://github.com/zero-editor/zero/releases/download/v#{version}/zero_aarch64.dmg"
   name "zero"
